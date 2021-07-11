c132dff1e2ac57dfa26182ea2aa9fa11c3a46b51
w79021655 <jyunwu@gamania.com>
2021-07-11 15:46:57 +0800

    First commit
    First commit

---
+++ app/src/main/java/com/sample/launchmode/MainActivity4.kt
@@ -0,0 +1,80 @@
+package com.sample.launchmode
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.sample.launchmode.databinding.ActivityMain4Binding
+
+class MainActivity4 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain4Binding
+    object KEY {
+        var TAG: String = MainActivity4::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain4Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.e(KEY.TAG, "onCreate")
+
+        binding.button.setOnClickListener {
+            val intent = Intent()
+            intent.setClass(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonB.setOnClickListener {
+            val intent = Intent()
+            intent.setClass(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonC.setOnClickListener {
+            val intent = Intent()
+            intent.setClass(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonD.setOnClickListener {
+            val intent = Intent()
+            intent.setClass(this, MainActivity4::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Log.e(KEY.TAG, "onNewIntent")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(KEY.TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(KEY.TAG, "onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e(KEY.TAG, "onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(KEY.TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(KEY.TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(KEY.TAG, "onDestroy")
+    }
+}
